f97d112239f808283a915588e44c8f63a92a653f
Lucky <51163246+LaxmanHJ@users.noreply.github.com>
2020-02-26 18:41:59 +0530

    Update pympler_memoryLeak.py

--- profiling/pympler_memoryLeak.py
+++ profiling/pympler_memoryLeak.py
@@ -1,11 +1,1 @@
-from pympler.classtracker import ClassTracker
-
-import Factory
-
-tracker=ClassTracker()
-tracker=track_class(Factory.main1)
-print ("Create data")
-tracker.create_snapshot()
-data = create_data(tracker)
-tracker.create_snapshot()
 
